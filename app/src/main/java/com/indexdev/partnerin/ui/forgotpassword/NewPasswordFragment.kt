ea1dd9ad3f73622e03250c599a3b59903157bb49
Musnadil <firdaus.musnadil@gmail.com>
2023-04-27 11:41:25 +0700

    ui new password

---
+++ app/src/main/java/com/indexdev/partnerin/ui/forgotpassword/NewPasswordFragment.kt
@@ -0,0 +1,25 @@
+package com.indexdev.partnerin.ui.forgotpassword
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.indexdev.partnerin.databinding.FragmentNewPasswordBinding
+
+class NewPasswordFragment : Fragment() {
+    private var _binding: FragmentNewPasswordBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNewPasswordBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
